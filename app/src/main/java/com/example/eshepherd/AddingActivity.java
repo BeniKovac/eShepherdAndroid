8d31110f82616b4cf48d30a5118cd4d636fefbd9
Benjamin <beni.kovac@gmail.com>
2020-12-18 20:50:24 +0100

    Initial commit EMPeShepherd

---
+++ app/src/main/java/com/example/eshepherd/AddingActivity.java
@@ -0,0 +1,18 @@
+package com.example.eshepherd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AddingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding);
+    }
+
+    public void launchOvceActivity(View view) {
+    }
+}
---
+++ app/src/main/java/com/example/eshepherd/OvceActivity.java
@@ -0,0 +1,14 @@
+package com.example.eshepherd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OvceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ovce);
+    }
+}
