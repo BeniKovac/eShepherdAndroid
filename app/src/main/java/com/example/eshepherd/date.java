415a90ee053c36d2311399cb833064498a5ddf0c
Benjamin <beni.kovac@gmail.com>
2020-12-20 22:59:35 +0100

    Narejeno sortiranje po datumu in po številki ovce pri seznamu Ovc. Novi razred "date" pomaga pri primerjanju datumov.

---
+++ app/src/main/java/com/example/eshepherd/date.java
@@ -0,0 +1,17 @@
+package com.example.eshepherd;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class date {
+    public static boolean isValidDate(String inDate) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        try {
+            dateFormat.parse(inDate.trim());
+        } catch (ParseException pe) {
+            return false;
+        }
+        return true;
+    }
+}
